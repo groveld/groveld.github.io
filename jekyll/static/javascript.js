dcdae8fb84350bd9538475bb07699f01f035242b
Martin Groeneveld <groveld@gmail.com>
2016-08-20 19:50:33 +0200

    updated .php path

--- jekyll/static/javascript.js
+++ jekyll/static/javascript.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
         .show();
 
       $.ajax({
-        url: '{{ site.baseurl }}/static/php/mail.php',
+        url: '{{ site.baseurl }}/static/sendmail.php',
         type: 'POST',
         data: $form.serialize(),
         dataType: 'json',
@@ -114,33 +114,5 @@ $(document).ready(function() {
         }
       });
     });
-
-  function replaceAllWords(newWord) {
-    for (var i = 0; i < document.childNodes.length; i++) {
-      checkNode(document.childNodes[i]);
-    }
-    function checkNode(node) {
-      var nodeName = node.nodeName.toLowerCase();
-      if(nodeName === 'script' || nodeName === 'style') {return;}
-      if (node.nodeType === 3) {
-        var text = node.nodeValue;
-        var newText = text.replace(/\b\w+/g, newWord);
-        node.nodeValue = newText;
-      }
-      if (node.childNodes.length > 0) {
-        for (var j = 0; j < node.childNodes.length; j++) {
-          checkNode(node.childNodes[j]);
-        }
-      }
-    }
-  }
-  var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-  $(document).keydown(function(e) {
-    kkeys.push( e.keyCode );
-    if ( kkeys.toString().indexOf( konami ) >= 0 ) {
-      $(document).unbind('keydown',arguments.callee);
-      replaceAllWords("alpaca");
-    }
-  });
 
 });
