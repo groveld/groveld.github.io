0e0211dcb641789cec129197447e2959052af19f
Martin Groeneveld <martin@groveld.com>
2018-06-01 13:40:55 +0200

    getting caching to work

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/public/js/service-worker.js', {scope: '/'}).then(function(registration) {
+    navigator.serviceWorker.register('/public/js/sw.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
