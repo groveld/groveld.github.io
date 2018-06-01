5806c7a0e1ca674dcf3e8e1140b73cf20ef995b6
Martin Groeneveld <martin@groveld.com>
2018-06-01 21:11:05 +0200

    getting caching to work

--- src/service-worker.js
+++ src/service-worker.js
@@ -26,21 +26,23 @@ const cacheFiles = [
   '/articles/ohm2013-observe-hack-make'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(cacheFiles);
-    })
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(cacheFiles);
+      })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
 });
 
-self.addEventListener('fetch', (event) => {
-  // event.respondWith(
-  //   caches.match(event.request).then(function(response) {
-  //     return response || fetch(event.request);
-  //   })
-  // );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
