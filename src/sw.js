db288c75145d613d7796665704f371cca07a54a0
Martin Groeneveld <martin@groveld.com>
2018-06-02 13:00:41 +0200

    getting caching to work

--- src/sw.js
+++ src/sw.js
@@ -2,6 +2,11 @@
 
 const cacheName = 'groveld-cache';
 const cacheFiles = [
+  'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
+  'https://code.jquery.com/jquery-3.3.1.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
   '/',
   '/blog',
   '/about',
