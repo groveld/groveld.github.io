0a656db45692219817d79dec7b8dc75a4371f859
Martin Groeneveld <martin@groveld.com>
2018-06-03 10:02:39 +0200

    getting caching to work

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,18 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('ServiceWorker registration successful with scope:', registration.scope);
-    }).catch(err => {
-      console.log('ServiceWorker registration failed:', err);
-    });
-  });
-}
-
-window.addEventListener('beforeinstallprompt', async e => {
-  await e.prompt();
-});
-
-
 $(document).ready(function() {
 
   $('#show-comments').click(function() {
