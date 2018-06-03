15950206bed710e84ae7bbbb5d122921f85fe6ab
Martin Groeneveld <martin@groveld.com>
2018-06-03 14:01:44 +0200

    getting caching to work

--- src/sw.js
+++ src/sw.js
@@ -1,46 +1,44 @@
+---
+permalink: /sw.js
+---
+
 'use strict';
 
-const cacheName = 'groveld-cache';
-const cacheFiles = [
-  'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
-  'https://code.jquery.com/jquery-3.3.1.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
-  '/',
-  '/?utm_source=homescreen',
-  '/blog',
-  '/about',
-  '/contact',
-  '/legal/terms',
-  '/legal/privacy',
-  '/legal/cookies',
-  '/static/css/style.css',
-  '/static/js/main.js',
-  '/static/images/logo.png',
-  '/articles/htaccess-snippets',
-  '/articles/www-non-www-redirection',
-  '/articles/give-user-permission-to-edit-and-add-files-in-var-www',
-  '/articles/use-gitolite-to-control-access-to-a-git-server',
-  '/articles/open-hackerspaces-day-2014',
-  '/articles/how-to-use-gpg-to-encrypt-and-sign-messages',
-  '/articles/postfix-with-mysql-backend-and-tls',
-  '/articles/clean-urls-with-jekyll-apache',
-  '/articles/what-it-really-means-to-hack',
-  '/articles/teamspeak-3-server-on-debian-ubuntu',
-  '/articles/ohm2013-observe-hack-make'
-];
+const urlsToCache = ['/?utm_source=homescreen'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(cacheFiles);
+// Cache assets
+{% for file in site.static_files %}urlsToCache.push('{{ file.path }}')
+{% endfor %}
+// Cache posts
+{% for post in site.posts %}urlsToCache.push('{{ post.url }}')
+{% endfor %}
+// Cache pages
+{% for page in site.html_pages %}{% if page.url != '/404' %}urlsToCache.push('{{ page.url }}')
+{% endif %}{% endfor %}
+
+const cacheName = 'groveld-cache-v3';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(event) {
+  console.log('Activating Service Worker ....', event);
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
