ea604de6eee9ab3f30de4bfc6467591f291cb577
Martin Groeneveld <martin@groveld.com>
2018-06-03 14:48:08 +0200

    getting caching to work

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,6 @@ permalink: /sw.js
 'use strict';
 
 const urlsToCache = ['/?utm_source=homescreen'];
-
 // Cache assets
 {% for file in site.static_files %}urlsToCache.push('{{ file.path }}')
 {% endfor %}
@@ -13,21 +12,42 @@ const urlsToCache = ['/?utm_source=homescreen'];
 {% for post in site.posts %}urlsToCache.push('{{ post.url }}')
 {% endfor %}
 // Cache pages
-{% for page in site.html_pages %}{% if page.url != '/404' or page.url != '/CNAME' %}urlsToCache.push('{{ page.url }}')
+{% for page in site.html_pages %}{% if page.url != '/404' %}urlsToCache.push('{{ page.url }}')
 {% endif %}{% endfor %}
 
-const cacheName = 'groveld-cache-v3';
+const cacheName = `groveld-{{ site.time | date: '%s' }}`;
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(cacheName)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!cacheName.includes(key)) return caches.delete(key);
+      })
+    ))
+  );
 });
 
 self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(res) {
+          return caches.open(cacheName).then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    })
+  );
 });
