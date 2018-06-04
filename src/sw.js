5d973d52be048c91fbc3021092e7b447da6f1430
Martin Groeneveld <groveld@users.noreply.github.com>
2018-06-04 10:46:17 +0200

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -31,17 +31,4 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        let clone = response.clone();
-        caches.open(cacheName).then(function(cache) {
-          cache.put(event.request, clone);
-        });
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/404');
-    })
-  );
 });
