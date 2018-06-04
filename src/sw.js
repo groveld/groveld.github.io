029ffe964d37959c04d1d251f74ac58b63b4a952
Martin Groeneveld <groveld@users.noreply.github.com>
2018-06-04 10:09:21 +0200

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -4,25 +4,17 @@ permalink: /sw.js
 
 'use strict';
 
-const cacheName = `groveld-{{ site.time | date: '%s' }}`;
-const urlsToCache = ['/?utm_source=homescreen'];
-
-// Cache assets
-{% for file in site.static_files %}urlsToCache.push('{{ file.path }}')
-{% endfor %}
-// Cache posts
-{% for post in site.posts %}urlsToCache.push('{{ post.url }}')
-{% endfor %}
-// Cache pages
-{% for page in site.html_pages %}{% if page.url != '/404' %}urlsToCache.push('{{ page.url }}')
-{% endif %}{% endfor %}
+const cacheHash = {{ site.time | date: '%s' }};
+const CACHE_NAME = 'groveld-' + cacheHash;
+const urlsToCache = ['/?utm_source=homescreen','/static/images/logo.png'];
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -30,7 +22,7 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(keys) {
       return Promise.all(keys.map(function(key) {
-        if (cacheName.indexOf(key) === -1) {
+        if (CACHE_NAME.indexOf(key) === -1) {
           return caches.delete(key);
         }
       }));
@@ -40,16 +32,14 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        let clone = response.clone();
-        caches.open(cacheName).then(function(cache) {
-          cache.put(event.request, clone);
-        });
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/static/images/logo.png');
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
