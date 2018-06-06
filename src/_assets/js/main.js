4adcb116e922b4a6f767f344593975b87b12a428
Martin Groeneveld <martin@groveld.com>
2018-06-06 23:28:01 +0200

    getting caching to work

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -14,35 +14,35 @@ if ('serviceWorker' in navigator) {
 //   event.prompt();
 // });
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $('#show-comments').click(function() {
-    var disqus_shortname = 'groveld';
-    // ajax request to load the disqus javascript
-    $.ajax({
-      type: "GET",
-      url: "https://" + disqus_shortname + ".disqus.com/embed.js",
-      dataType: "script",
-      cache: true
-    });
-    $('#comments').fadeIn(150);
-    // hide the button once comments load
-    $(this).fadeOut(150);
-  });
+//   $('#show-comments').click(function() {
+//     var disqus_shortname = 'groveld';
+//     // ajax request to load the disqus javascript
+//     $.ajax({
+//       type: "GET",
+//       url: "https://" + disqus_shortname + ".disqus.com/embed.js",
+//       dataType: "script",
+//       cache: true
+//     });
+//     $('#comments').fadeIn(150);
+//     // hide the button once comments load
+//     $(this).fadeOut(150);
+//   });
 
-  //Check to see if the window is top if not then display button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 300) {
-      $('#scrollUp').fadeIn(150);
-    } else {
-      $('#scrollUp').fadeOut(150);
-    }
-  });
+//   //Check to see if the window is top if not then display button
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 300) {
+//       $('#scrollUp').fadeIn(150);
+//     } else {
+//       $('#scrollUp').fadeOut(150);
+//     }
+//   });
 
-  //Click event to scroll to top
-  $('#scrollUp').click(function() {
-    $('html, body').animate({scrollTop: 0}, 250);
-    return false;
-  });
+//   //Click event to scroll to top
+//   $('#scrollUp').click(function() {
+//     $('html, body').animate({scrollTop: 0}, 250);
+//     return false;
+//   });
 
-});
+// });
