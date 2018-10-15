43b7537dab5df6c3c7aad8c5c83fe5ad00692ea2
Martin Groeneveld <martin@groveld.com>
2018-10-15 21:13:38 +0200

    publish test

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,16 @@
-// Register Service Worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//     // ServiceWorker registration successful
-//   }).catch(function(err) {
-//     console.log('ServiceWorker registration failed:', err);
-//   });
-// }
+---
+---
+
+{% if jekyll.environment == 'production' %}
+Register Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // ServiceWorker registration successful
+  }).catch(function(err) {
+    console.log('ServiceWorker registration failed:', err);
+  });
+}
+{% endif %}
 
 var vhFix = new vhFix([
   {
