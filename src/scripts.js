1affc3da094d8e3fd76429df77bfd4e5d811afd3
Martin Groeneveld <martin@groveld.com>
2018-10-15 21:22:49 +0200

    publish test

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@
 ---
 
 {% if jekyll.environment == 'production' %}
-Register Service Worker
+// Register Service Worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // ServiceWorker registration successful
