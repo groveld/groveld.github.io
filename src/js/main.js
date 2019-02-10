9e91fda70eaa656e2a0fb5f096cf50efe49ae630
Martin Groeneveld <martin@groveld.com>
2019-02-10 12:43:48 +0100

    minor changes, PWA logo renaming and Lighthouse tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,10 @@
 {% if jekyll.environment == 'production' %}
 // Register Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw2.js').then(function(registration) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // ServiceWorker registration successful
   }).catch(function(err) {
+    // ServiceWorker registration failed
     console.log('ServiceWorker registration failed:', err);
   });
 }
