2b3ca07e7b52c34e8f685d2eba3968fbfe40204a
groveld <3328493+groveld@users.noreply.github.com>
2025-11-10 11:29:32 +0100

    Update contact.js (#137)
    * Update contact.js
    
    * Fix formatting in .jscpd.json

--- functions/contact.js
+++ functions/contact.js
@@ -1,185 +1,134 @@
 const formatEmailBody = (name, email, subject, message, domain) => {
-	return `
+  return `
 		<b>${name}</b>&nbsp;-&nbsp;${email}<br><br>
-    <b>${subject}</b><br><br>
-    ${message}<br><br>
-    --&nbsp;<br>
-    <i>Dit bericht is verzonden via het contactformulier op ${domain}</i>
-  `;
+		<b>${subject}</b><br><br>
+		${message}<br><br>
+		--&nbsp;<br>
+		<i>Dit bericht is verzonden via het contactformulier op ${domain}</i>
+	`;
 };
 
 export const onRequestPost = async (context) => {
-  try {
-    return await handleRequest(context);
-  } catch (_err) {
-    return jsonResponse("Er is iets misgegaan", 500);
-  }
+	try {
+		return await handleRequest(context);
+	} catch (_err) {
+		return jsonResponse("Er is iets misgegaan", 500);
+	}
 };
 
 const sanitizeInput = (input) => {
-  const map = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#039;",
-    "\n": "<br>",
-  };
+	const map = {
+		"&": "&amp;",
+		"<": "&lt;",
+		">": "&gt;",
+		'"': "&quot;",
+		"'": "&#039;",
+		"\n": "<br>",
+};
 
-  return String(input).replace(/[&<>"'\n]/g, (m) => map[m]);
+	return String(input).replace(/[&<>"'\n]/g, (m) => map[m]);
 };
 
 const jsonResponse = (message, status = 200) => {
-  const success = status >= 200 && status < 300;
-  return new Response(JSON.stringify({ success, message }), {
-    status: status,
-    headers: { "Content-Type": "application/json" },
-  });
+	const success = status >= 200 && status < 300;
+	return new Response(JSON.stringify({ success, message }), {
+		status: status,
+		headers: { "Content-Type": "application/json" },
+	});
 };
 
 const handleRequest = async ({ request, env }) => {
-  const formData = await request.formData();
-  const sanitizedData = new FormData();
+	const formData = await request.formData();
+	const sanitizedData = new FormData();
 
-  for (const [key, value] of formData.entries()) {
-    sanitizedData.append(key, sanitizeInput(value));
-  }
+	for (const [key, value] of formData.entries()) {
+		sanitizedData.append(key, sanitizeInput(value));
+	}
 
-  // Check if phone (honeypot) field is filled
-  const phone = sanitizedData.get("phone");
-  if (phone && phone.trim() !== "") {
-    return jsonResponse("Spam gedetecteerd", 400);
-  }
+	// Check if phone (honeypot) field is filled
+	const phone = sanitizedData.get("phone");
+	if (phone && phone.trim() !== "") {
+		return jsonResponse("Spam gedetecteerd", 400);
+	}
 
-  const name = sanitizedData.get("name");
-  const email = sanitizedData.get("email");
-  const subject = sanitizedData.get("subject");
-  const message = sanitizedData.get("message");
-  const domain = request.headers.get("Host");
-  const token = sanitizedData.get("cf-turnstile-response");
-  const ip = request.headers.get("CF-Connecting-IP");
+	const name = sanitizedData.get("name");
+	const email = sanitizedData.get("email");
+	const subject = sanitizedData.get("subject");
+	const message = sanitizedData.get("message");
+	const domain = request.headers.get("Host");
+	const token = sanitizedData.get("cf-turnstile-response");
+	const ip = request.headers.get("CF-Connecting-IP");
 
-  if (!name || !email || !subject || !message) {
-    return jsonResponse("Ontbrekende verplichte velden", 400);
-  }
+	if (!name || !email || !subject || !message) {
+		return jsonResponse("Ontbrekende verplichte velden", 400);
+	}
 
-  const isTokenValid = await validateToken(env, token, ip);
-  if (!isTokenValid.success) {
-    return jsonResponse("Ongeldige token", 403);
-  }
+	const isTokenValid = await validateToken(env, token, ip);
+	if (!isTokenValid.success) {
+		return jsonResponse("Ongeldige token", 403);
+	}
 
-  const emailResponse = await sendEmailWithMailgun(
-    env,
-    name,
-    email,
-    subject,
-    message,
-    domain
-  );
-  if (!emailResponse.success) {
-    return jsonResponse("Fout bij verzenden", 500);
-  }
+	const emailResponse = await sendEmailWithMailgun(
+		env,
+		name,
+		email,
+		subject,
+		message,
+		domain
+	);
+	if (!emailResponse.success) {
+		return jsonResponse("Fout bij verzenden", 500);
+	}
 
-  return jsonResponse("Bericht succesvol verzonden", 200);
+	return jsonResponse("Bericht succesvol verzonden", 200);
 };
 
 const sendRequest = async (url, options) => {
-  const response = await fetch(url, options);
-  return { success: response.ok, status: response.status };
+	const response = await fetch(url, options);
+	return { success: response.ok, status: response.status };
 };
 
 const validateToken = async (env, token, ip) => {
-  const formData = new FormData();
-  formData.append("secret", env.TURNSTILE_SECRET_KEY);
-  formData.append("response", token);
-  formData.append("remoteip", ip);
+	const formData = new FormData();
+	formData.append("secret", env.TURNSTILE_SECRET_KEY);
+	formData.append("response", token);
+	formData.append("remoteip", ip);
 
-  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
-  const options = {
-    method: "POST",
-    body: formData,
-  };
+	const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+	const options = {
+		method: "POST",
+		body: formData,
+	};
 
-  const response = await sendRequest(url, options);
-  return response;
+	const response = await sendRequest(url, options);
+	return response;
 };
 
 const sendEmailWithMailgun = async (
-  env,
-  name,
-  email,
-  subject,
-  message,
-  domain
+	env,
+	name,
+	email,
+	subject,
+	message,
+	domain
 ) => {
-  const formData = new FormData();
-  formData.append(
-    "from",
-    `${env.MAILGUN_FROM_NAME} <${env.MAILGUN_FROM_EMAIL}>`
-  );
-  formData.append(
-    "h:Sender",
-    `${env.MAILGUN_FROM_NAME} <${env.MAILGUN_FROM_EMAIL}>`
-  );
-  formData.append(
-    "to",
-    `${env.MAILGUN_TO_NAME} <${env.MAILGUN_TO_EMAIL}>`
-  );
-  formData.append("h:Reply-To", `${name} <${email}>`);
-  formData.append("subject", `${name} - ${subject}`);
-  formData.append(
-    "html",
-    formatEmailBody(name, email, subject, message, domain)
-  );
+	const formData = new FormData();
+	formData.append("from", `${env.MAILGUN_FROM_NAME} <${env.MAILGUN_FROM_EMAIL}>`);
+	formData.append("h:Sender", `${env.MAILGUN_FROM_NAME} <${env.MAILGUN_FROM_EMAIL}>`);
+	formData.append("to", `${env.MAILGUN_TO_NAME} <${env.MAILGUN_TO_EMAIL}>`);
+	formData.append("h:Reply-To", `${name} <${email}>`);
+	formData.append("subject", `${name} - ${subject}`);
+	formData.append("html", formatEmailBody(name, email, subject, message, domain));
 
-  const url = env.MAILGUN_API_URI;
-  const options = {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${btoa(`api:${env.MAILGUN_API_KEY}`)}`,
-    },
-    body: formData,
-  };
+	const url = env.MAILGUN_API_URI;
+	const options = {
+		method: "POST",
+		headers: {
+			Authorization: `Basic ${btoa(`api:${env.MAILGUN_API_KEY}`)}`,
+		},
+		body: formData,
+	};
 
-  const response = await sendRequest(url, options);
-  return response;
+	const response = await sendRequest(url, options);
+	return response;
 };
-
-// const sendEmailWithSendGrid = async (
-//   env,
-//   name,
-//   email,
-//   subject,
-//   message,
-//   domain
-// ) => {
-//   const url = env.SENDGRID_API_URI;
-//   const body = JSON.stringify({
-//     personalizations: [
-//       {
-//         to: [{ email: env.SENDGRID_TO_EMAIL, name: env.SENDGRID_TO_NAME }],
-//         subject: `${name} - ${subject}`,
-//       },
-//     ],
-//     from: { email: env.SENDGRID_FROM_EMAIL, name: env.SENDGRID_FROM_NAME },
-//     reply_to: { email: email, name: name },
-//     content: [
-//       {
-//         type: "text/html",
-//         value: formatEmailBody(name, email, subject, message, domain),
-//       },
-//     ],
-//   });
-
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${env.SENDGRID_API_KEY}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: body,
-//   };
-
-//   const response = await sendRequest(url, options);
-//   return response;
-// };
